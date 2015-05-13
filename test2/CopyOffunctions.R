7580ef96d95b4695b6be3dfd25eebfc4e4b7dbc7
Melsteroni <frazier@nceas.ucsb.edu>
2015-05-13 11:24:58 -0700

    change to csv_compare merge conflict

--- test2/CopyOffunctions.R
+++ test2/CopyOffunctions.R
@@ -2,7 +2,7 @@
 # changing argument names
 
   # csv comparison function, made global
-  csv_compare <<- function(o, step, prefix=sprintf2('temp/%s_MAR', basename(getwd()))){
+  csv_compare <<- function(o, step, prefix=sprintf15('temp/%s_MAR', basename(getwd()))){
     
     dir_temp = basename(dirname(prefix))
     if (!file.exists(dir_temp)) dir.create(dir_temp, recursive=T)
