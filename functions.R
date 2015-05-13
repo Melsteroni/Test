635186aaaf3616e91732917021e091d7ce419889
Melsteroni <melanie.frazier@gmail.com>
2015-05-13 15:50:56 -0700

    Revert "Change C2"
    This reverts commit 7f22df33cda24bdff2411d869ca6669ac27765ea.

--- functions.R
+++ functions.R
@@ -15,9 +15,6 @@ write.csv(newData, 'test.csv')
 
 # changing argument names
 
-
-Change C2
-
   # csv comparison function, made global
   csv_compare <<- function(o, step, prefix=sprintf3('temp/%s_MAR', basename(getwd()))){
     
